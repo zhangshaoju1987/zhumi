57701c1d050d7449452f80784b4567fc33fe41a5
zhangshaoju1987 <zhangshaoju.12@gmail.com>
2024-09-02 08:32:06 -0700

    rewr

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Notify from "./components/Notifications";
-import { Notification, NotificationCompletion, Notifications, Registered, RegistrationError } from 'react-native-notifications';
-
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
 import {store,persistor} from './redux/store';
@@ -17,54 +15,9 @@ import {Example} from "./pages/Chat";
 import UserProfile from './pages/UserProfile';
 import * as settingsAction from "./redux/actions/settingsAction";
 import { PersistGate } from 'redux-persist/integration/react';
-import { NotificationActionResponse } from 'react-native-notifications/lib/dist/interfaces/NotificationActionResponse';
 
 
 const Stack = createStackNavigator();
-
-/**
- * 
- */
-function registNotification(){
-  Notifications.registerRemoteNotifications();
-  Notifications.events().registerRemoteNotificationsRegistered((event: Registered) => {
-      // TODO: Send the token to my server so it could send back push notifications...
-      console.log("Device Token Received", event.deviceToken);
-  });
-  Notifications.events().registerRemoteNotificationsRegistrationFailed((event: RegistrationError) => {
-      console.error(event);
-  });
-
-  Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion: (response: NotificationCompletion) => void) => {
-    console.log("Notification Received - Foreground", notification.payload);
-    // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
-    completion({alert: true, sound: true, badge: false});
-  });
-
-  Notifications.events().registerNotificationOpened((notification: Notification, completion: () => void, action: NotificationActionResponse) => {
-    console.log("Notification opened by device user", notification.payload);
-    console.log(`Notification opened with an action identifier: ${action.identifier} and response text: ${action.text}`);
-    completion();
-  });
-      
-  Notifications.events().registerNotificationReceivedBackground((notification: Notification, completion: (response: NotificationCompletion) => void) => {
-    console.log("Notification Received - Background", notification.payload);
-
-    // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
-    completion({alert: true, sound: true, badge: false});
-  });
-}
-
-Notifications.ios.checkPermissions().then((currentPermissions) => { 
-  console.log('Badges enabled: ' + !!currentPermissions.badge);
-  console.log('Sounds enabled: ' + !!currentPermissions.sound);
-  console.log('Alerts enabled: ' + !!currentPermissions.alert);
-  console.log('Car Play enabled: ' + !!currentPermissions.carPlay);
-  console.log('Critical Alerts enabled: ' + !!currentPermissions.criticalAlert);
-  console.log('Provisional enabled: ' + !!currentPermissions.provisional);
-  console.log('Provides App Notification Settings enabled: ' + !!currentPermissions.providesAppNotificationSettings);
-  console.log('Announcement enabled: ' + !!currentPermissions.announcement);
-});
 
 //registNotification();
 export default class App extends React.Component {
@@ -80,11 +33,11 @@ export default class App extends React.Component {
                   <Stack.Navigator initialRouteName="Home" screenListeners={({route,navigation}) => ({
                       state: (e) => {
                         // Do something with the state
-                        console.log("回到页面：",route.name,',state changed', e.data);
+                        // console.log("回到页面：",route.name,',state changed', e.data);
                         // Do something with the `navigation` object
                         //https://reactnavigation.org/docs/navigation-events#screenlisteners-prop-on-the-navigator
                         if(!navigation.canGoBack()) {
-                          console.log("we're on the initial screen");
+                          //console.log("we're on the initial screen");
                           store.dispatch(settingsAction.setHideHomeFAB(false))
                         }
                       },
