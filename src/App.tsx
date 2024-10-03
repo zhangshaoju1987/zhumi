e21047489a60d2a5de111462bb71ff92721c8441
zhangshaoju1987 <zhangshaoju.12@gmail.com>
2024-10-03 22:27:33 +0800

    智能助手

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Notify from "./components/Notifications";
-import { Provider as PaperProvider } from 'react-native-paper';
+import { Button, IconButton, Provider as PaperProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
 import {store,persistor} from './redux/store';
 import ThemeOptions from "./Theme";
@@ -17,6 +17,7 @@ import UserProfile from './pages/UserProfile';
 import * as settingsAction from "./redux/actions/settingsAction";
 import { PersistGate } from 'redux-persist/integration/react';
 import { TaskAddView } from './pages/area_monitor/TaskAddView';
+import { Alert } from 'react-native';
 
 const Stack = createStackNavigator();
 
@@ -47,8 +48,16 @@ export default class App extends React.Component {
                   })}>
                     <Stack.Screen name="Home" component={Home}  options={{headerShown: false,title:"首页"}}/>
                     <Stack.Screen name="Chat" component={Example}  options={{headerShown: true,title:"智能助手"}}/>
-                    <Stack.Screen name="AreaTask" component={AreaTask}  options={{headerShown: true,title:"电子围栏"}}/>
-                    <Stack.Screen name="TaskAddView" component={TaskAddView} options={({ route }) => ({ headerShown: true,title: (route.params as any)?.title||"任务设置" })}/>
+                    <Stack.Screen name="AreaTask" component={AreaTask}  options={(route)=>({headerShown: true,title:"电子围栏",headerRight: () => (
+                                      <IconButton iconColor='green' icon={"bell-plus"}  onPress={() => Alert.alert("新增")}/>
+                                    ),})}/>
+                    <Stack.Screen name="TaskAddView" component={TaskAddView} options={({ route }) => ({ 
+                                    headerShown: true,
+                                    title: (route.params as any)?.title||"任务设置",
+                                  }  
+                                )}
+                    
+                    />
                     <Stack.Screen name="Map" component={App1}  options={{headerShown: true,title:"地图案例"}}/>
                     <Stack.Screen name="UserProfile" component={UserProfile}  options={{headerShown: true,title:"配置中心"}}/>
                   </Stack.Navigator>
